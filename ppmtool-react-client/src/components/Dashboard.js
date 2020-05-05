ea67f669e5d72eb54284c77526ca196880d78fb3
Kai Tian <tian.kai@husky.neu.edu>
2020-05-05 13:42:11 -0700

    finish section5

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -12,11 +12,6 @@ class Dashboard extends Component {
   render() {
     const { projects } = this.props.project;
 
-    const projectObject = {
-      projectName: "ProjectName PROPS",
-      projectIdentifier: "PROP",
-      description: "description from PROPS"
-    };
     return (
       <div className="projects">
         <div className="container">
@@ -27,7 +22,7 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              {projects.map(project => (
+              {projects.map((project) => (
                 <ProjectItem key={project.id} project={project} />
               ))}
             </div>
@@ -40,11 +35,11 @@ class Dashboard extends Component {
 
 Dashboard.protoTypes = {
   project: PropTypes.object.isRequired,
-  getProjects: PropTypes.func.isRequired
+  getProjects: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  project: state.project
+const mapStateToProps = (state) => ({
+  project: state.project,
 });
 
 export default connect(mapStateToProps, { getProjects })(Dashboard);
